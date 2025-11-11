158e7524f3c64769d44d6441fdabc829a1206946
ChristySchott <christyhauschild@gmail.com>
2025-11-11 12:02:02 -0300

    chore: remove server

--- apps/server/eslint.config.js
+++
@@ -1,9 +0,0 @@
-import nodeConfig from '@insurance-mfe/eslint-config/node'
-import { defineConfig } from 'eslint/config'
-
-export default defineConfig([
-  {
-    ignores: ['dist', 'node_modules'],
-  },
-  ...nodeConfig,
-])
