3e916b2c0535651b944a80af7ac1ac9294f73bb1
ChristySchott <christyhauschild@gmail.com>
2025-11-11 12:12:05 -0300

    feat: update StepWizard component

--- apps/multicotador-host/src/components/StepWizard.tsx
+++ apps/multicotador-host/src/components/StepWizard.tsx
@@ -1,13 +1,13 @@
 import { Suspense, ReactNode } from 'react'
-import { useCotacaoStore } from '../store/cotacaoStore'
 import { StepProgress } from './StepProgress'
 import { Navigation } from './Navigation'
 import { LoadingSpinner } from './LoadingSpinner'
-import { Step1 } from '../steps/Step1'
-import { Step2 } from '../steps/Step2'
-import { Step3 } from '../steps/Step3'
-import { Step4 } from '../steps/Step4'
+import { Step1 } from './steps/Step1'
+import { Step2 } from './steps/Step2'
+import { Step3 } from './steps/Step3'
+import { Step4 } from './steps/Step4'
 import { ErrorBoundary } from './ErrorBoundary'
+import { useCotacaoStore } from '@/hooks/useCotacaoStore'
 
 const TOTAL_STEPS = 4
 
@@ -23,20 +23,14 @@ const renderStep = (currentStep: number) => {
 }
 
 export function StepWizard() {
-  const {
-    currentStep,
-    setCurrentStep,
-    cpf,
-    productType,
-    step2IsValid,
-    step3IsValid,
-  } = useCotacaoStore()
+  const { currentStep, setCurrentStep, cpf, productType, step2IsValid } =
+    useCotacaoStore()
 
   const canGoNext = () => {
     const rulesToGoNextBasedOnStep: Record<number, boolean> = {
       1: cpf.length > 0 && productType !== null,
       2: step2IsValid,
-      3: step3IsValid,
+      3: true,
       4: false,
     }
 
